632eb60bde5f457544fa527e3c1fea137e3a0ae4
godbrand69 <you@example.com>
2024-03-08 20:42:12 +0530

    homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,7 +9,7 @@ import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
-    <div id='home' className='w-full text-white pt-10 h-screen text-center'>
+    <div id='home' className='w-full text-white pt-10 text-center'>
 
       <video src={require("../../public/video1.mp4")} autoPlay muted loop className='video' />
 
